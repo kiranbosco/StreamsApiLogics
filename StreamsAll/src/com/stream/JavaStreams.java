ae4853474a3ee46ba2d16fa40e5d2e543a176668
Superman computers <Superman computers@DESKTOP-AGHAT19>
2018-03-04 23:21:28 +0530

    new Class

---
+++ StreamsAll/src/com/stream/JavaStreams.java
@@ -0,0 +1,78 @@
+package com.stream;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import javafx.scene.shape.Path;
+
+public class JavaStreams {
+	public static void main(String[] args)throws Exception {
+
+		/*		IntStream.range(1,10)	
+		.forEachOrdered(x-> System.out.print(x));
+		System.out.println();
+
+		//if you want skip some values use it 
+		IntStream.range(1,10)
+		.skip(5)
+		.forEachOrdered(x-> System.out.print(x));
+		System.out.println();
+		 */
+		//if you want add values
+		/*	System.out.println(
+				IntStream.range(1, 5)
+				.sum());
+		System.out.println();
+
+		 */
+
+		//if you want sorting findFirst
+		/*		Stream.of("ava","aneri","albetro","Abhi")
+		.sorted()
+		.findFirst()
+		.ifPresent(System.out::print);
+		 */
+		// filter any one charactor starts with ("s")
+
+		/*	String[]names={"kiran" ,"vijay", "kamal", "varsha" , "varun","kanalkannan"};
+		Arrays.stream(names)
+		.filter(x -> x.startsWith("v"))
+		.forEach(System.out::println);*/
+
+		//multiple operation
+/*
+			Arrays.stream(new int[]{2,4,6,8,10})
+		.map(x-> x* x)
+		.forEach(System.out::println);
+		 */
+
+		//print the values uppercause
+/*
+		List<String>list=Arrays.asList("paul","poloris","ibm","mastech");
+		list.stream()
+		.map(String::toUpperCase)
+		.filter(y-> y.startsWith("p"))
+		.forEach(System.out::println);*/
+		
+		//Stream row from the text files
+	/*	Stream<String>bands=Files.lines(Paths.get("/StreamsAll/src/com/stream/files.txt"));
+		bands.sorted()
+		.filter(x-> x.contains("kiran"))
+		.collect(Collectors.toList())
+		.forEach(x->System.out.println(x));*/
+		
+		
+		
+		
+		
+		
+	}
+
+
+
+}
